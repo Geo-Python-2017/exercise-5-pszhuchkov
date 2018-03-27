9fc0a9378251ae8d473882483e249dab0c37c1ab
pszhuchkov <pzhuchkov@gmail.com>
2018-03-27 15:29:12 +0400

    Added operations for data manipulations

--- data_exploration.py
+++ data_exploration.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 # Read the csv file
 data = pd.read_csv('6153237444115dat.csv', na_values=['*', '**', '***', '****', '*****', '******'])
-
+"""
 # How many rows is there in the data?
 print('The number of rows is',len(data))
 
@@ -25,3 +25,25 @@ print('\nThe standard deviation of the Maximum temperatures is',round(data['MAX'
 
 # How many unique stations exists in the data? (USAF column)
 print('\nThe amount of the unique stations is',len(data['USAF'].unique()))
+"""
+
+# Select columns
+selected = data[['USAF','YR--MODAHRMN','TEMP','MAX','MIN']]
+
+# Remove all rows from selected that has NoData in column TEMP
+selected = selected.dropna(subset=['TEMP'])
+
+# Convert the Fahrenheit temperatures from TEMP into a new column Celsius
+# Round the values in Celsius to have 0 decimals
+# Convert the Celsius values into integers
+selected['Celsius'] = ((selected['TEMP'] - 32) / 1.8).round(0).astype(int)
+"""
+# Round the values in Celsius to have 0 decimals
+selected['Celsius'] = selected['Celsius'].round(0)
+
+# Convert the Celsius values into integers
+selected['Celsius'] = selected['Celsius'].astype(int)
+"""
+# Output result
+print(selected)
+
