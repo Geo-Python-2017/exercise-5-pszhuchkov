7fd6e4c4a2e8be14e520762c7225c692cb362e96
pszhuchkov <pzhuchkov@gmail.com>
2018-03-28 13:22:44 +0400

    Updated the script after solving of the problem 4.

--- data_exploration.py
+++ data_exploration.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 The script presents some information about the input csv file.
-Then the script chooses certain columns and delete rows that contains NaN-values.
-Furthermore happens converting Fahrenheit temperatures to Celsius into new comumn.
-Then the dataFrame divides to two parts for every observation station.  
+The script chooses certain columns and delete rows that contains NaN-values.
+Happens converting Fahrenheit temperatures to Celsius into new comumn.
+The dataFrame divides into two parts for every observation station.
+The obtained dataframes divides  into some parts for different monthes (May and June).
+Among received dataframes happens counting mean, minimal and maximal temperatures and prints it to the screen. 
+ 
 
 Author: Pavel Zhuchkov - 27.03.2018
 """
@@ -52,18 +55,31 @@ kumpula.to_csv('Kumpula_temps_May_Aug_2017.csv',sep=',',float_format="%.2f")
 # Save rovaniemi DataFrame into csv file
 rovaniemi.to_csv('Rovaniemi_temps_May_Aug_2017.csv',sep=',',float_format="%.2f")
 
+# The median temperature in Helsinki Kumpula
+print('\nThe median temperature in Helsinki Kumpula has been',kumpula['TEMP'].median())
 
+# The median temperature in Rovaniemi
+print('\nThe median temperature in Rovaniemi has been',rovaniemi['TEMP'].median())
 
+# Select rows from kumpula and rovaniemi Dataframes for May only
+kumpula_may = kumpula.ix[(kumpula['YR--MODAHRMN'] >= 201705010000) & (kumpula['YR--MODAHRMN'] < 201706010000)]
+rovaniemi_may = rovaniemi.ix[(rovaniemi['YR--MODAHRMN'] >= 201705010000) & (rovaniemi['YR--MODAHRMN'] < 201706010000)]
 
+# Select rows from kumpula and rovaniemi Dataframes for June only
+kumpula_june = kumpula.ix[(kumpula['YR--MODAHRMN'] >= 201706010000) & (kumpula['YR--MODAHRMN'] < 201707010000)]
+rovaniemi_june = rovaniemi.ix[(rovaniemi['YR--MODAHRMN'] >= 201706010000) & (rovaniemi['YR--MODAHRMN'] < 201707010000)]
 
+# Output mean temperatures for May and June in Helsinki Kumpula and Rovaniemi
+print('\n The mean temperature for May in Helsinki Kumpula is',round(kumpula_may['TEMP'].mean(),2), ', in Rovaniemi is',round(rovaniemi_may['TEMP'].mean(),2))
+print('\n The mean temperature for June in Helsinki Kumpula is',round(kumpula_june['TEMP'].mean(),2), ', in Rovaniemi is',round(rovaniemi_june['TEMP'].mean(),2))
 
+# Output min temperatures for May and June in Helsinki Kumpula and Rovaniemi
+print('\n The minimal temperature for May in Helsinki Kumpula is',kumpula_may['TEMP'].min(), ', in Rovaniemi is',rovaniemi_may['TEMP'].min())
+print('\n The minimal temperature for June in Helsinki Kumpula is',kumpula_june['TEMP'].min(), ', in Rovaniemi is',rovaniemi_june['TEMP'].min())
 
-
-
-
-
-
-
+# Output max temperatures for May and June in Helsinki Kumpula and Rovaniemi
+print('\n The maximal temperature for May in Helsinki Kumpula is',kumpula_may['TEMP'].max(), ', in Rovaniemi is',rovaniemi_may['TEMP'].max())
+print('\n The maximal temperature for June in Helsinki Kumpula is',kumpula_june['TEMP'].max(), ', in Rovaniemi is',rovaniemi_june['TEMP'].max())
 
 
 
